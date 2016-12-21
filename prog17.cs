bf09964bf98fe8cad65c1d5c7425feb93690cd35
Calvin Cheung <thisiscalvinccs@gmail.com>
2016-12-21 16:02:43 +0800

    Add files via upload

---
+++ prog17.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+// http://forum.brackeys.com/thread/answer-to-the-challenge-in-how-to-program-in-c-nr-4-2/
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
+		{
+			// multidimensional arrays
+			CreateGrid();
+
+			Console.ReadKey();
+		}
+		public static void CreateGrid()
+		{
+			int width = 20;
+			int height = 20;
+
+			int[][] grid = new int[width][height];
+		}
+	}
+}
