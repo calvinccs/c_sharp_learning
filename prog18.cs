69838d7891bd67bbcd2c4982e46268679c298c8b
Calvin Cheung <thisiscalvinccs@gmail.com>
2016-12-21 16:02:56 +0800

    Add files via upload

---
+++ prog18.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+// http://forum.brackeys.com/thread/answer-to-the-challenge-in-how-to-program-in-c-nr-4-2/
+
+namespace FirstConsoleProject
+{
+	class Animal
+	{
+		string name = "Spotty";
+		int age = 6;
+		float happiness = 0.5f;
+
+
+		                   
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
+		{
+			// 
+		}
+	}
+}
