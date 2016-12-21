c0926e7091f737265da81706c1920b9cfeb774d3
Calvin Cheung <thisiscalvinccs@gmail.com>
2016-12-21 10:21:36 +0800

    Add files via upload

---
+++ prog09.cs
@@ -0,0 +1,19 @@
+﻿using System;
+// http://forum.brackeys.com/thread/answer-to-the-challenge-in-how-to-program-in-c-nr-4-2/
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
+		{
+			int i = 1;
+			while (i <= 10) {
+				Console.WriteLine(i);
+				i += 1;
+			}
+			Console.ReadKey();
+		}
+	}
+}
+
