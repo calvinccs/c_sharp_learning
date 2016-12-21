57ad6b17adb0c8095fd78bb9a3dd52b7a7f36428
Calvin Cheung <thisiscalvinccs@gmail.com>
2016-12-21 16:02:16 +0800

    Add files via upload

---
+++ prog15.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+// http://forum.brackeys.com/thread/answer-to-the-challenge-in-how-to-program-in-c-nr-4-2/
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
+		{
+			//
+			List<int> numbers = new List<int> ();
+
+			numbers.Add(13);
+			numbers.Add(4);
+			numbers.Add(8);
+
+			//Console.WriteLine(numbers[1]);
+
+			for (int i = 0; i < numbers.Count; i++)
+			{
+				Console.Write(numbers[i] + ", ");
+			}
+
+			Console.WriteLine();
+			numbers.RemoveAt(0); //removed the first number
+
+			for (int i = 0; i < numbers.Count; i++)
+			{
+				Console.Write(numbers[i] + ", ");
+			}
+
+			//foreach (string name in names)
+			//{
+			//	Console.Write(name + ", ");
+			//}
+
+			Console.ReadKey();
+		}
+	}
+}
+
