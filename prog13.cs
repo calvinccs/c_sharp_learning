47b7270bf395db7ff0223063dc568252342ae054
Calvin Cheung <thisiscalvinccs@gmail.com>
2016-12-21 16:01:46 +0800

    Add files via upload

---
+++ prog13.cs
@@ -0,0 +1,30 @@
+﻿using System;
+// http://forum.brackeys.com/thread/answer-to-the-challenge-in-how-to-program-in-c-nr-4-2/
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
+		{
+			//ARRAY 
+			int[] numbers = new int[5];
+			numbers[0] = 12;
+			numbers[1] = 3;
+			numbers[2] = 5;
+
+			//for (int i = 0; i < numbers.Length; i++)
+			//{
+			//	Console.Write(numbers[i] + ", ");
+			//}
+
+			foreach (int number in numbers)
+			{
+				Console.Write(number + ", ");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
+
