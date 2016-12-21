a683232613f0c31229997a7cdb9ecd3541b3e00e
Calvin Cheung <thisiscalvinccs@gmail.com>
2016-12-21 10:19:40 +0800

    Add files via upload

---
+++ prog05.cs
@@ -0,0 +1,34 @@
+﻿using System;
+// http://forum.brackeys.com/thread/answer-to-the-challenge-in-how-to-program-in-c-nr-4-2/
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
+		{
+			int num01 = 16;
+
+			int num02 = 4;
+			Console.WriteLine("What is " + num01 + " times " + num02 + "?");
+			int answer = Convert.ToInt32(Console.ReadLine());
+			if (answer == num01 * num02) {
+
+				Console.WriteLine("Well done! Your answer is correct.");
+
+			}
+			else {
+
+				Console.WriteLine("Are you even trying?");
+
+			}
+			Console.ReadKey();
+
+
+
+
+		}
+	}
+}
+
+
