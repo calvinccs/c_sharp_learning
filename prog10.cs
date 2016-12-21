3a7c8bd188f44e9bd14fca0a875939dd96b7a6d5
Calvin Cheung <thisiscalvinccs@gmail.com>
2016-12-21 10:22:20 +0800

    Add files via upload

---
+++ prog10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// http://forum.brackeys.com/thread/answer-to-the-challenge-in-how-to-program-in-c-nr-4-2/
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args) //this is a method called 'Main'. It is called when the program starts
+		{
+			Random numberGen = new Random();
+
+			int numberOFAttempts = 0;
+			int attempt = 0;
+
+			while (attempt != 6) {
+				attempt = numberGen.Next(1, 7);
+				Console.WriteLine("Tom rolled: " + attempt + ".");
+				numberOFAttempts++;
+			}
+			Console.WriteLine("It too
+			Console.ReadKey();
+		}
+	}
+}
+
